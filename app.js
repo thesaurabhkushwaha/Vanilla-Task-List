d34aa20255d72b306d020e2b756877fba5434458
Saurabh Kushwaha <saurabh.sprk@gmail.com>
2022-04-09 20:03:29 +0530

    All functionalities implemented - HTML structure, Add/Remove tasks, Clear all tasks, Filter tasks.

---
+++ app.js
@@ -0,0 +1,76 @@
+// Define UI variables
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearButton = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners(){
+    //Add task event
+    form.addEventListener('submit',addTask);
+    //Remove task event
+    taskList.addEventListener('click', removeTask);
+    //Clear task event
+    clearButton.addEventListener('click', clearTasks);
+    // Filter tasks event
+    filter.addEventListener('keyup', filterTasks);
+}
+
+
+// Add task
+function addTask(e){
+    if(taskInput.value === '')
+        alert('Add a valid task')
+
+    // Else Create a new li element to display on task-list
+    const li = document.createElement('li');
+    // Add class
+    li.className='collection-item';
+    // Create a text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new link element for deleting
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    // Add icon for delete link
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Add link to current li
+    li.appendChild(link);
+    // Add li to ul -> append list item to task list
+    taskList.appendChild(li);
+    // Clear input
+    taskInput.value="";
+
+    e.preventDefault();
+}
+
+// Remove task
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm("Are you sure?"))
+            e.target.parentElement.parentElement.remove();
+    }
+}
+
+// Clear task list
+function clearTasks(){
+    while(taskList.firstChild)
+        taskList.removeChild(taskList.firstChild);
+}
+
+// Filter tasks
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(
+        (task) => {
+            const item = task.textContent;
+            if(item.toLowerCase().indexOf(text) != -1){
+                task.style.display='block';
+            } else{
+                task.style.display='none';
+            }
+        }
+    )
+}
